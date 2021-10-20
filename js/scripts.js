4935d602ca17740c3bc78adcabc3f2132297092b
marco-gioto <marcogioto.mg@gmail.com>
2021-10-20 10:52:14 -0300

    paintOver

--- js/scripts.js
+++ js/scripts.js
@@ -1,55 +1,56 @@
 
 const innerWidth = window.innerWidth;
 const appHeight = window.innerHeight;
-const cellWidth = innerWidth / 100 ;
-const cellHeight = innerWidth / 100 ;
+const cellWidth = innerWidth / 100;
+const cellHeight = innerWidth / 100;
 const tabRows = parseInt(appHeight / cellWidth);
 const tableCells = 100 * tabRows;
 const table = document.getElementById('table-cont');
-const colors = ['red','blue','yellow','pink','green'];
+const colors = ['red', 'blue', 'yellow', 'pink', 'green'];
 const modalEl = document.querySelector('.modal');
 let colorSelected = 'red';
 let estructura = '';
 
-for(let i = 0; i< tableCells; i++){
-       
-        estructura += `<div id="${i}" class="cellItem"  style="width: ${cellWidth}px; height: ${cellHeight}px; border: .5px solid black;"> </div>`
+for (let i = 0; i < tableCells; i++) {
+
+    estructura += `<div id="${i}" class="cellItem"  style="width: ${cellWidth}px; height: ${cellHeight}px; border: .5px solid black;"> </div>`
 }
 table.innerHTML = estructura;
 
 const cellItems = document.querySelectorAll('.cellItem');
 
-for(let i = 0; i < cellItems.length ; i++){
-   cellItems[i].addEventListener('click',function(ev)  {
-        
-       setItemColor(cellItems[i]);
+for (let i = 0; i < cellItems.length; i++) {
+    cellItems[i].addEventListener('click', function (ev) {
+
+        setItemColor(cellItems[i], false);
     });
-    cellItems[i].addEventListener('contextmenu',function(ev){
+    cellItems[i].addEventListener('contextmenu', function (ev) {
         ev.preventDefault();
-        console.log(ev);
-        generateModalColors(ev.pageX,ev.pageY);
+        generateModalColors(ev.pageX, ev.pageY);
     });
-    cellItems[i].addEventListener('mouseover',function(ev){
-       
-        if(ev.buttons === 1 || ev.buttons === 3){
-            setItemColor(cellItems[i]);
+    cellItems[i].addEventListener('mouseover', function (ev) {
+
+        if (ev.buttons === 1 || ev.buttons === 3) {
+            setItemColor(cellItems[i], true);
         }
-        
+
     });
-    
+
 
 }
 
 /**
  * 
- * Función que recibe un elemento html y setea su color y en caso de que ya tengo un color seteado lo remueve.
- * @param {htmlElement} element 
+ * Función que recibe un elemento html y setea su color , en caso de que paintOver sea false le que ya tengo un color seteado lo remueve y en paintOver sea true
+ * este cambia el color por el nuevo elegido.
+ * @param {HTMLElement} element 
+ * @param {boolean} paintOver
  */
-const setItemColor = (element) => {
-    if( element.style.getPropertyValue('background')){
+const setItemColor = (element, paintOver) => {
+    if (element.style.getPropertyValue('background') && !paintOver) {
         element.style.removeProperty('background');
-    }else{
-        element.style.background= colorSelected;
+    } else {
+        element.style.background = colorSelected;
     }
 };
 
@@ -59,13 +60,13 @@ const setItemColor = (element) => {
  * @param {number} mousePositionX 
  * @param {number} mousePositionY 
  */
-const generateModalColors = (mousePositionX,mousePositionY) => {
-   
+const generateModalColors = (mousePositionX, mousePositionY) => {
+
     const colorsList = document.getElementById('colors-list');
     let listItems = '';
 
-    for(let i = 0; i < colors.length; i++){
-        listItems += `<li><button class="btn-color ${colors[i] === colorSelected ? 'active': ''}" onclick="setGlobalColor('${colors[i]}')" style="background:${colors[i]};" onclick="setColor(${colors[i]})"></button></li>`;
+    for (let i = 0; i < colors.length; i++) {
+        listItems += `<li><button class="btn-color ${colors[i] === colorSelected ? 'active' : ''}" onclick="setGlobalColor('${colors[i]}')" style="background:${colors[i]};" onclick="setColor(${colors[i]})"></button></li>`;
     }
     colorsList.innerHTML = listItems;
     modalEl.style.removeProperty('display');
@@ -77,7 +78,7 @@ const generateModalColors = (mousePositionX,mousePositionY) => {
 /**
  * Función que detecta cuando el mouse dejo la modal de colores y la cierra.
  */
-modalEl.addEventListener('mouseleave',function(ev){
+modalEl.addEventListener('mouseleave', function (ev) {
 
     closeModal();
 });
@@ -85,9 +86,9 @@ modalEl.addEventListener('mouseleave',function(ev){
 /**
  * Función para cerrar la modal de colores;
  */
-const closeModal = ()=> {
+const closeModal = () => {
     modalEl.style.display = 'none';
-}
+};
 
 /**
  * Función que recibe el color elegido para setearlo como nuevo color global.
@@ -96,4 +97,4 @@ const closeModal = ()=> {
 const setGlobalColor = (color) => {
     colorSelected = color;
     closeModal();
-}
+};
